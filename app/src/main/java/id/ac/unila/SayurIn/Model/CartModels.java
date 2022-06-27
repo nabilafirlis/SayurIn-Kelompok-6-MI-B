b109650d669074e85d6b9f8b89d5c844da8a6908
nabilafirlis <95564998+nabilafirlis@users.noreply.github.com>
2022-06-27 18:45:20 +0700

    Update CartModels.java

--- app/src/main/java/id/ac/unila/SayurIn/Model/CartModels.java
+++ app/src/main/java/id/ac/unila/SayurIn/Model/CartModels.java
@@ -21,3 +21,64 @@ public class CartModels {
         this.cartModels = new ArrayList<CartModels>();
     }
     
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getHarga() {
+        return harga;
+    }
+
+    public void setHarga(int harga) {
+        this.harga = harga;
+    }
+
+    public int getTotalOrder() {
+        return totalOrder;
+    }
+
+    public void setTotalOrder(int totalOrder) {
+        this.totalOrder = totalOrder;
+    }
+
+    public int getJumlahPesanan() {
+        return jumlahPesanan;
+    }
+
+    public void setJumlahPesanan(int jumlahPesanan) {
+        this.jumlahPesanan = jumlahPesanan;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public List<CartModels> getCartModels() {
+        return cartModels;
+    }
+
+    public void setCartModels(List<CartModels> cartModels) {
+        this.cartModels = cartModels;
+    }
+
+    public void addCart(CartModels orderModels){
+        this.cartModels.add(orderModels);
+    }
+}
