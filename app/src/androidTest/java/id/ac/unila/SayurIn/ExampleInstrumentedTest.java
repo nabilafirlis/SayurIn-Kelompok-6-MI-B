63a77ed635c83f5169dda09d770cd45bedacf7d2
nabilafirlis <95564998+nabilafirlis@users.noreply.github.com>
2022-06-27 18:40:16 +0700

    Update ExampleInstrumentedTest.java

--- app/src/androidTest/java/id/ac/unila/SayurIn/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/id/ac/unila/SayurIn/ExampleInstrumentedTest.java
@@ -22,6 +22,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
 
-        assertEquals("id.ac.polinema.aplikasi_msi", appContext.getPackageName());
+        assertEquals("id.ac.unila,SayurIn", appContext.getPackageName());
     }
 }
